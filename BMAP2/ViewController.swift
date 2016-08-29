bddbf88aa26d0f659855cf25284a6c00a2d6d018
Carlos Heil <CarlosHeil@carlos-macbook-air.local>
2016-08-29 10:25:23 -0400

    Initial Commit

---
+++ BMAP2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BMAP2
+//
+//  Created by Carlos Heil on 8/29/16.
+//  Copyright © 2016 Bose Corporation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
